31bf3bd53bc40a656b9534871ed6492edad48866
arunima1403 <131650060+arunima1403@users.noreply.github.com>
2023-04-25 11:27:51 +0530

    Create TestWorld.java

---
+++ TestWorld.java
@@ -0,0 +1,9 @@
+
+/** 
+ * The TestWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class TestWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, testing pipeline!");
+  }
+}
